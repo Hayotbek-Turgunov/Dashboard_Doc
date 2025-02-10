672d681997b23f109d1516d0b0da9d6e5f78d2c0
Hayotbek-Turgunov <hayotbekturgunov01@gmail.com>
2025-02-10 05:06:22 +0500

    Media bug fix

--- app/api-reference/page.tsx
+++ app/api-reference/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 export default function ApiReference() {
   return (
-    <main className="max-w-4xl mx-auto pl-0 pr-4 sm:p-6 lg:p-8">
+    <main className="max-w-4xl mx-auto pl-0 pr-8 sm:p-6 lg:p-8">
       <h1 className="text-3xl sm:text-4xl font-bold mb-6 text-center">
         API Reference
       </h1>
